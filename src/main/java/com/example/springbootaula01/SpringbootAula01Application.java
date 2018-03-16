c18fc0f06df9fee601bfd3b0b96c8ca09cf816cb
paulovictor11 <pauloalmeidajp652@gmail.com>
2018-03-15 22:00:54 -0300

    Aula Roteiro 2 - Ainda em teste

---
+++ src/main/java/com/example/springbootaula01/SpringbootAula01Application.java
@@ -0,0 +1,12 @@
+package com.example.springbootaula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAula01Application.class, args);
+	}
+}
